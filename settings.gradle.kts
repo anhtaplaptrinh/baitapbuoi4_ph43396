092658fab70d092a1d334251ddce9e3516caa3b3
acertuananh <anhttph43396@fpt.edu.vn>
2024-05-15 23:19:17 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "baitapbuoi4_ph43396"
+include(":app")
+ 
